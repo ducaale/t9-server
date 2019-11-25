6969b2a304b3cacd0debcadff582c2288961bda1
ducaale <sharaf.13@hotmail.com>
2019-11-25 19:46:34 +0000

    add validation

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express()
 
 app.get('/', (req, res) => {
   const { input } = req.query
+  if (!/^[2-9]*$/.test(input)) {
+    return res.status(400).send({
+      message: 'input should be a numeric between 2-9'
+    })
+  }
   const possibleWords = t9.predictWord(input)
   res.send(possibleWords)
 })
